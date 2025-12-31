2e1863fb7fd150f79b9a6f35cc45e72af4f7437b
Chamidu Gunathunga <chamidugunathunga1212@gmail.com>
2025-12-31 13:46:31 +0530

    done the data transformation part

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,72 +1,14 @@
-# import os
-# import sys
-# import pandas as pd
-
-# from src.exception import CustomeException
-# from src.logger import logging
-
-# from sklearn.model_selection import train_test_split
-# from dataclasses import dataclass
-
-
-# @dataclass
-# class DataIngestionConfig:
-#     train_data_path : str=os.path.join('artifacts','train.csv')
-#     test_data_path : str=os.path.join('artifacts','test.csv')
-#     raw_data_path : str=os.path.join('artifacts','data.csv')
-
-
-# class DataIngestion:
-#     def __init__(self):
-#         self.ingestion_config = DataIngestionConfig()
-
-
-#     def initiate_data_config(self):
-#         logging.info('Entered the data ingestion or components')
-
-#         try:
-#             df = pd.read_csv('notebook/data/stud.csv')
-#             logging.info('Read the data set as the Dataframe')
-
-#             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-
-#             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
-
-#             logging.info('Train Test data is initiated...')
-
-#             train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
-
-#             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-#             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-
-#             logging.info('Data ingestion is completed...')
-
-#             return(
-#                 self.ingestion_config.train_data_path,
-#                 self.ingestion_config.test_data_path
-#             )
-
-
-#         except Exception as e:
-#             raise CustomeException(e,sys)    
-        
-
-# if __name__ == "main":
-#     obj = DataIngestion()
-#     train_path,test_path= obj.initiate_data_config()
-            
-
-
-
 import os
 import sys
 import pandas as pd
 
-from src.exception import CustomeException
+from src.exception import CustomException
 from src.logger import logging
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
 
 
 @dataclass
@@ -123,9 +65,11 @@ class DataIngestion:
             )
 
         except Exception as e:
-            raise CustomeException(e, sys)
+            raise CustomException(e, sys)
 
 
 if __name__ == "__main__":
     obj = DataIngestion()
     train_path, test_path = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_path,test_path)
